63389f7601c3f6e3fcc217e5241e12a3096b56f4
corey redding and Leah Sherrell <corey@>
2017-04-27 10:55:08 -0700

    branch styles using addClass based on user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ $(function() {
   var height = parseInt(prompt("How tall are you in feet?"));
 
   if (height >= 5) {
-    $("#over5").show();
+    $(".over5").addClass("bg-success");
   } else if (height < 5) {
-    $("#under5").show();
+    $(".under5").addClass("bg-success");
   }
 });
